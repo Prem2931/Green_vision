1fde193950fc92b010ef51427fd0d7a622460ec9
Prem2931 <mrudprem@gmail.com>
2025-08-03 18:55:53 +0530

    ml_project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "green_vision",
+    version = "0.0.1",
+    author = "prem_sagar",
+    packages=find_packages(),
+    install_requires = []
+)
